ff2a3e5802c3b828e75f3575bdcabdc92609bb8b
Your Name <Your@email.com>
2012-07-12 19:00:40 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InitialFantasy::Application.config.secret_token = '73784164b53e5042b27b00fd98971676dc393409196798c50860f28fde2d84ca94d85d7988004588069fa200e5724ba40428d2c2cb8bc35e10d72efa292d3556'
