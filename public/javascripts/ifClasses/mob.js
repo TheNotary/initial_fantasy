cfff68542623b3ab1f0b9b6f59d406b5b2172765
TheNotary <no.email@plz.com>
2012-07-30 02:37:05 -0600

    really cruising along with the logic and classes of the battle screen.  I just started a convention where 'public' methods shall start with a capital letter, and 'private' will be lower case, just to make it look nicer.

---
+++ public/javascripts/ifClasses/mob.js
@@ -0,0 +1,61 @@
+function Mob(id, image){
+  this.id = 1;
+  this.img = image;
+  this.position = BattlePositions.position1; // position on the battle screen...
+  
+  this.stats = getStats(id);
+}
+
+function getStats(id){
+  // ajax querie to pull down stats from database...
+  var combatStats = new CombatStats(0, 0, 0, 1, 1);
+  
+  return combatStats;
+}
+
+BattlePositions = {
+  position1: [1, 1],
+  position2: [1, 10]
+}
+
+
+
+
+function CombatStats(str, stam, attack, hp, mp){
+  this.str = str;
+  this.stam = stam;
+  this.attack = attack;
+  this.hp = hp;
+  this.mp = mp;
+  
+  this.immunities = new Immunities(["fire", "ice"]);
+  this.absorbtions = "";
+  this.weaknesses = "";
+  this.combatActions = "";
+}
+
+
+function Immunities(list){
+  this.fire = (jQuery.inArray("fire", list) != -1) ? false: true;  // oh man this is ugly, but atleast on one line...
+  this.ice = (jQuery.inArray("ice", list) != -1) ? false: true;  
+}
+
+function Absorbtions(){
+  this.fire = false;
+  this.ice = false;
+  this.thunder = true;
+}
+
+
+function CombatActions(){
+  this.attack = true;
+  this.spells = "";
+  this.mobAttacks = new MobAttacks();
+}
+
+function MobAttacks(){
+  
+}
+
+
+
