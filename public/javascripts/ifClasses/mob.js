d2dbeb8414ad3a8225f7ad286d12a9f479017a8e
TheNotary <no.email@plz.com>
2012-07-30 14:14:10 -0600

    The enemies are showing up pretty good on the canvas.  Now it's time to work on the heroes.

--- public/javascripts/ifClasses/mob.js
+++ public/javascripts/ifClasses/mob.js
@@ -1,13 +1,17 @@
-function Mob(id, image, position){
+function Mob(id, image, position, type){
   this.id = 1;
   this.image = image;
+  this.shadowImages = [imgflightShadow0, imgflightShadow1, imgflightShadow2, imgflightShadow3];
   this.position = position; // position on the battle screen...
-  this.actualX = BattlePositions.position1[0];  // for debugging...
+  this.x = 0;  // these variables are for hovering/ flying effects and misc. toneberry shananagins
+  this.y = 0; 
+  
+  this.type = type;
   
   this.stats = getStats(id);
-  
-  
 }
+
+// this.type is almost like "effect"...  I can have a flying effect, then a ghost effect, then a burning effect, etc...
 
 function getStats(id){
   // ajax querie to pull down stats from database...
@@ -17,10 +21,47 @@ function getStats(id){
 }
 
 BattlePositions = {
-  position1: [368, 200],   // 200 + 199 = 399      // FIXME:  This should be indexed according to bottom right corner, not top since size veries, draw feet on ground
-  position2: [255, 103]      // 103 + 96 =  199
+  position1: [368, 296],         
+  position2: [255, 199],      
+  position3: [111, 253]
 }
 
+
+Mob.prototype.drawUnit = function(offsetForSlideIn){
+  switch(this.type){
+    case "ground":
+    case undefined:
+      this.drawSimpleGroundUnit(offsetForSlideIn);
+      break;
+    case "flying":
+      this.drawFlyingUnit(offsetForSlideIn);
+      break;
+  }
+  
+}
+
+
+// TODO:  add acceleration and add a delay between each mob so they come one at a time almost.  
+Mob.prototype.drawSimpleGroundUnit = function(offsetForSlideIn){
+  var calculatedX = this.position[0] + offsetForSlideIn;
+    // Draw the mobs up...
+    battleScreen.context.drawImage(this.image,
+      calculatedX, this.position[1] - this.image.height,
+      this.image.width, this.image.height);
+}
+
+Mob.prototype.drawFlyingUnit = function(offsetForSlideIn){
+  var calculatedX = this.position[0] + offsetForSlideIn;
+  
+  battleScreen.context.drawImage(this.image,
+      calculatedX, this.position[1] - this.image.height-50,
+      this.image.width, this.image.height);
+      
+  // draw shadow now too...
+  battleScreen.context.drawImage(this.shadowImages[0],
+    calculatedX-12, this.position[1] + 20,
+    this.shadowImages[0].width, this.shadowImages[0].height);
+}
 
 
 
