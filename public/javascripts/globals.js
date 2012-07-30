0cf5f6e889879affc7489597da8a536f6645d68b
TheNotary <no.email@plz.com>
2012-07-30 06:09:21 -0600

    finished refactoring... took a little longer due to needing to move keyboard.js to the bottom of the declarations on the html.

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -1,2 +1,5 @@
 var currentScreen;  // this variable tracks the current UI being displayed...  It's useful for the render loop to know what to render...  We'll see how that turns out when I have more code down...
 
+var titleScreen;
+var battleScreen;
+
