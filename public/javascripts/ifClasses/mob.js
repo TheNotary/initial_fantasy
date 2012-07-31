44c558808659f4391825bcaa7741213a8c366c0f
TheNotary <no.email@plz.com>
2012-07-31 01:41:10 -0600

    about to do refactor to put draw unit into the unit class.

--- public/javascripts/ifClasses/mob.js
+++ public/javascripts/ifClasses/mob.js
@@ -6,15 +6,8 @@ function Mob(type, id, image, position, stance){
   
 }
 
-
 Mob.prototype = new Unit();
 
-
-BattlePositions = {
-  position1: [368, 296],         
-  position2: [255, 199],      
-  position3: [111, 253]
-}
 
 
 Mob.prototype.drawUnit = function(offsetForSlideIn){
@@ -27,8 +20,9 @@ Mob.prototype.drawUnit = function(offsetForSlideIn){
       this.drawFlyingUnit(offsetForSlideIn);
       break;
   }
-  
 }
+
+
 
 
 // TODO:  add acceleration and add a delay between each mob so they come one at a time almost.  
@@ -53,44 +47,6 @@ Mob.prototype.drawFlyingUnit = function(offsetForSlideIn){
     this.shadowImages[0].width, this.shadowImages[0].height);
 }
 
-
-
-function CombatStats(str, stam, speed, attack, hp, mp){
-  this.str = str;
-  this.stam = stam;
-  this.speed = speed;
-  this.attack = attack;
-  this.hp = hp;
-  this.mp = mp;
-  
-  this.immunities = new Immunities(["fire", "ice"]);
-  this.absorbtions = "";
-  this.weaknesses = "";
-  this.combatActions = "";
-}
-
-
-function Immunities(list){
-  this.fire = (jQuery.inArray("fire", list) != -1) ? false: true;  // oh man this is ugly, but atleast on one line...
-  this.ice = (jQuery.inArray("ice", list) != -1) ? false: true;  
-}
-
-function Absorbtions(){
-  this.fire = false;
-  this.ice = false;
-  this.thunder = true;
-}
-
-
-function CombatActions(){
-  this.attack = true;
-  this.spells = "";
-  this.mobAttacks = new MobAttacks();
-}
-
-function MobAttacks(){
-  
-}
 
 
 
--- public/javascripts/ifClasses/unit.js
+++ public/javascripts/ifClasses/unit.js
@@ -6,6 +6,7 @@ function Unit(type, id, image, position, stance){
   this.position = position; // position on the battle screen...
   this.x = 0;  // these variables are for hovering/ flying effects and misc. toneberry shananagins
   this.y = 0; 
+  this.mirrorY = false;  // set this to true to turn an enemy facing the other way...
   
   // this.stance is almost like "effect"...  I can have a flying effect, then a ghost effect, then a burning effect, etc...
   this.stance = stance;      // "ground", "flying", "burning", "ghost/ Transparent", ...
@@ -20,6 +21,16 @@ unitType = {
   critter: 2
 }
 
+enemyPositions = {
+  position1: [368, 296],         
+  position2: [255, 199],      
+  position3: [111, 253]
+}
+
+partyPositions = {
+  row1Front: [500, 199],
+  row1Back: [550, 199]
+}
 
 
 Unit.getBaseStats = function(type, id){
@@ -29,5 +40,50 @@ Unit.getBaseStats = function(type, id){
   
   return combatStats;
 }
+
+
+
+
+
+
+function CombatStats(str, stam, speed, attack, hp, mp){
+  this.str = str;
+  this.stam = stam;
+  this.speed = speed;
+  this.attack = attack;
+  this.hp = hp;
+  this.mp = mp;
+  
+  this.immunities = new Immunities(["fire", "ice"]);
+  this.absorbtions = "";
+  this.weaknesses = "";
+  this.combatActions = "";
+}
+
+
+function Immunities(list){
+  this.fire = (jQuery.inArray("fire", list) != -1) ? false: true;  // oh man this is ugly, but atleast on one line...
+  this.ice = (jQuery.inArray("ice", list) != -1) ? false: true;  
+}
+
+function Absorbtions(){
+  this.fire = false;
+  this.ice = false;
+  this.thunder = true;
+}
+
+
+function CombatActions(){
+  this.attack = true;
+  this.spells = "";
+  this.mobAttacks = new MobAttacks();
+}
+
+function MobAttacks(){
+  this.fight = true;
+  this.magic = ["fire", "ice", "lightning"];
+  this.mobAttacks = ["Goblin Kick"];
+}
+
 
 
