7b61aa611c93a6d0815abb46b678736c8c9997d0
TheNotary <no.email@plz.com>
2012-07-31 05:53:17 -0600

    made very scary change in globals.js and m.js.  I disabled the main loop by removing set interval in there, but then I use a window.onEachFrame(main) call that does the loop interval setting for us.

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -13,3 +13,31 @@ heroes.cecil = function(){}
 
 
 
+
+var onEachFrame;
+if (window.webkitRequestAnimationFrame) {
+  onEachFrame = function(cb) {
+    var _cb = function() { cb(); webkitRequestAnimationFrame(_cb); }
+    _cb();
+  };
+} else if (window.mozRequestAnimationFrame) {
+  onEachFrame = function(cb) {
+    var _cb = function() { cb(); mozRequestAnimationFrame(_cb); }
+    _cb();
+  };
+} else {
+  onEachFrame = function(cb) {
+    setInterval(cb, 1000 / 60);
+  }
+}
+
+window.onEachFrame = onEachFrame;
+
+
+$(function(){
+  window.onEachFrame(main);
+});
+
+
+
+
