470c17bd975338e49d705393e7fce9f12668079c
TheNotary <no.email@plz.com>
2012-07-31 14:01:14 -0600

    fleshing out the main loop... the guide I'm following doesn't explain it well.

--- public/javascripts/ifClasses/mob.js
+++ public/javascripts/ifClasses/mob.js
@@ -9,22 +9,15 @@ Mob.prototype = new Unit();
 
 
 
-Mob.prototype.calculateX = function(offsetForSlideIn){
-  return this.x + offsetForSlideIn;
-}
-
-
 // This method is somewhat specific to mobs vs heroes... so I'm not refactoring it up to the super
 Mob.prototype.drawFlyingUnit = function(offsetForSlideIn){
-  var calculatedX = this.position[0] + offsetForSlideIn;
-  
   battleScreen.context.drawImage(this.image,
-      calculatedX, this.y - this.image.height-50,
+      this.x, this.y - this.image.height-50,
       this.image.width, this.image.height);
       
   // draw shadow now too...
   battleScreen.context.drawImage(this.shadowImages[0],
-    calculatedX-12, this.y + 20,
+    this.x-12, this.y + 20,
     this.shadowImages[0].width, this.shadowImages[0].height);
 }
 
