9ff0c1164378fcdb3a2b39fa4818cdb99b06ef93
TheNotary <no@mail.plz>
2012-08-25 13:40:43 -0600

    working system I think... in the middle of refactoring getters and setters for heroQueue... need to figure out how to get it to just work.

--- public/javascripts/ifClasses/hero.js
+++ public/javascripts/ifClasses/hero.js
@@ -5,6 +5,7 @@ function Hero(type, id, image, position, stance, row){
   this.htmlElements = new HtmlElements(this.name, row);
   this.heroIsReady = false;  // waitbar drawn at 100% yet?.. this variable is read from within the draw loop, not the update loop.  Careful...
   this.isSelected = false;  // owner of the action bar items in the combat menu
+                            // when we remove something from the heroQueue... we need to set this variable on the next hero... so the refactoring will be elsewhere...
 }
 
 
